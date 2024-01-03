5a96d25c2bbb1edbdc0bceaaea468ae1229d64b0
Mhkkks <142821082+Mhkkks@users.noreply.github.com>
2024-01-03 21:47:56 +0530

    Create Q16.java

---
+++ A2_Patterns/Q16.java
@@ -0,0 +1,32 @@
+package test;
+
+import java.util.Scanner;
+public class Assign2
+{
+    public static void main(String[] args)
+    {
+       Scanner sc = new Scanner(System.in);
+        
+            int n= sc.nextInt();
+            int d= (n-1)*2-1;
+            for(int i=1;i<=n;i++)
+            {
+                for(int j=1;j<=i;j++)
+                {
+                        System.out.print(j+" ");
+                }
+                for(int k=1;k<=d;k++)
+                {
+                    System.out.print("  ");
+                }
+                for(int j=i;j>=1;j--)
+                {   
+                    if(j==n)
+                        continue;
+                    System.out.print(j+" ");
+                }
+                d=d-2;
+                System.out.println();
+            }
+        }
+    }    
