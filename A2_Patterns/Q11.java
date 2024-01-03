458325fd025e9aa14a44428209a6ab857734b0f5
Mhkkks <142821082+Mhkkks@users.noreply.github.com>
2024-01-03 21:33:35 +0530

    Update Q11.java

--- A2_Patterns/Q11.java
+++ A2_Patterns/Q11.java
@@ -5,30 +5,17 @@ public class Assign2
 {
     public static void main(String[] args)
     {   
-        try(Scanner sc=new Scanner(System.in)){
-        int n=sc.nextInt();
-        for(int i=1;i<=n;i++)
-        {
-            System.out.print("*");
-        }
-        System.out.println();
-        for(int i=1;i<=n-2;i++)
-        {
-           for(int k=n-2;k>=i;k--)
-            {
-                System.out.print(" ");
-            }
-           for(int j=1;j<=i;j++)
-            {  
-                if(j==1)
-                System.out.print("*");
-            }
-           System.out.println();
-        }
-        for(int i=1;i<=n;i++)
-        {
-            System.out.print("*");
-        }
-    }
-}      
-}
+     try (Scanner sc = new Scanner(System.in))
+    	        { int n= sc.nextInt();
+    	            int d=1;
+    	            for(int i=1;i<=n;i++)
+    	            {
+    	                for(int j=1;j<=i;j++)
+    	                {
+    	                    System.out.print(d+++"  ");
+    	                }
+    	                System.out.println();
+    	            }
+    	        }
+    	    }
+    	}
