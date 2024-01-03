fd41c42abf1a77107a5a6152088dba29ca716862
Mhkkks <142821082+Mhkkks@users.noreply.github.com>
2024-01-03 19:42:31 +0530

    Update Q1.java

--- A2_Patterns/Q1.java
+++ A2_Patterns/Q1.java
@@ -1,1 +1,16 @@
+public class Assign2 {
+
+	public static void main(String[] args) {
+		for (int r = 0; r < 5; r++) {
+
+			for (int c = 0; c < r + 1; c++) {
+
+				System.out.print("*");
+			}
+
+			System.out.print("\n");
+
+		}
+	}
+}
 
